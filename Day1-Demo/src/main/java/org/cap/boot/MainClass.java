a3bcbd2b17826cac998e72fd3a22612b9b65d539
2018s <2018s@wellfire>
2020-03-26 16:00:19 +0530

    com 1

---
+++ Day1-Demo/src/main/java/org/cap/boot/MainClass.java
@@ -0,0 +1,11 @@
+package org.cap.boot;
+
+import org.cap.model.SalesClass;
+
+public class MainClass {
+	public static void main(String args[])
+	{
+		SalesClass obj=new SalesClass();
+		obj.print();
+	}
+}
---
+++ Day1-Demo/src/main/java/org/cap/model/SalesClass.java
@@ -0,0 +1,15 @@
+package org.cap.model;
+
+public class SalesClass {
+
+
+	int []arr= {12,3,4,5,6};
+	public void print()
+	{
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
+	}
+}
+
+
+
