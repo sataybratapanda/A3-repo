2455839621f727310536ec00a1569b2a4413f7ad
satyabratapanda@net <2016satyabratapanda@gmil.com>
2020-03-27 16:25:14 +0530

    enhanced for loop

--- Day1-Demo/src/main/java/org/cap/model/SalesClass.java
+++ Day1-Demo/src/main/java/org/cap/model/SalesClass.java
@@ -6,10 +6,17 @@ public class SalesClass {
 	int []arr= {12,3,4,5,6};
 	public void print()
 	{
-		for(int i=0;i<arr.length;i++)
-			System.out.println(arr[i]);
+		
+		
+			for(int num:arr)
+			{
+				System.out.println("the enhanced for loop");
+				System.out.println(num);
+			}
+			
+		}
 	}
-}
+
 
 
 
